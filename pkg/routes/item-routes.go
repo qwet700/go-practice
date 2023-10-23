762d3efc077907bf6857f7e1fcf9181fce1ef1cb
qwet700 <trungtnd@unicloud.com.vn>
2023-10-23 08:29:51 +0700

    t

--- pkg/routes/item-routes.go
+++ pkg/routes/item-routes.go
@@ -1,1 +1,12 @@
 package item_routes
+
+import (
+	"github.com/gorilla/mux"
+)
+
+var ImpItem = func(router *mux.Router) {
+	router.HandleFunc("/item/", controller.createItem).Methods("POST")
+	router.HandleFunc("/item/", controller.getItem).Methods("GET")
+	router.HandleFunc("/item/{id}", controller.getItemID).Methods("GET")
+
+}
